32ff6a6918d334f8e24b3dae5989227f584205ad
XT Lenovo <tranxuantruong420@gmail.com>
2024-11-29 14:42:16 +0700

    [UPDATE] - done footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/config.css";
 
+import Footer from "@/components/Layout/Footer";
 import ScrollToTop from "@/components/UI/ScrollToTop";
 import Loading from "@/components/UI/Loading";
 
 export default function RootLayout({ children }) {
-    return (
-        <html suppressHydrationWarning={true} lang="en">
-            <body>
-                {children}
-                <ScrollToTop />
-                <Loading />
-            </body>
-        </html >
-    );
+  return (
+    <html suppressHydrationWarning={true} lang="en">
+      <body>
+        {children}
+        <ScrollToTop />
+        <Loading />
+        <Footer />
+      </body>
+    </html>
+  );
 }
