346312e7c2e8614d036fbdcc4f8eda62f7856598
Kichirou <mrphuc244@gmail.com>
2024-12-01 21:37:18 +0700

    [ADD] - add Sec4

--- src/app/(pages)/home/IndexPage.jsx
+++ src/app/(pages)/home/IndexPage.jsx
@@ -4,12 +4,14 @@ import React from "react";
 
 import Sec1 from "./Sec1";
 import Sec2 from "./Sec2";
+import Sec4 from "./Sec4";
 
 export default function IndexPage() {
     return (
         <>
             <Sec1 />
             <Sec2 />
+            <Sec4 />
         </>
     )
 }
---
+++ src/app/(pages)/home/Sec4.jsx
@@ -0,0 +1,9 @@
+import TimeLineHistory1 from "@/components/UI/TimeLineHistory1"
+
+export default function Sec4() {
+    return (
+        <>
+            <TimeLineHistory1 />
+        </>
+    )
+}
