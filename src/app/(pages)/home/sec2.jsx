fdf43f9411dc54e128c5365f5df3037cba3dd992
XT Lenovo <tranxuantruong420@gmail.com>
2024-12-02 15:22:23 +0700

    [FIX] - fix title - fix image

--- src/app/(pages)/home/sec2.jsx
+++ src/app/(pages)/home/sec2.jsx
@@ -5,7 +5,7 @@ import "../../../styles/sec2.css";
 const BorderLight = () => {
   return (
     <div
-      className="bg-white mx-3 glow-div"
+      className="bg-white mx-3"
       style={{ width: "5px", height: "180px" }}
     ></div>
   );
@@ -64,9 +64,9 @@ export default function Sec1() {
               className="d-none d-md-block align-self-center glow-div"
               style={{ backgroundColor: "#fff", height: "4px", width: "27%" }}
             ></div>
-            <h1 className="text-white fw-bold text-center mx-5">
+            <p className="text-white fw-bold text-center mx-5 fs-1">
               NHỮNG CON SỐ NỔI BẬT
-            </h1>
+            </p>
             <div
               className="d-none d-md-block align-self-center glow-div"
               style={{ backgroundColor: "#fff", height: "4px", width: "27%" }}
@@ -80,155 +80,139 @@ export default function Sec1() {
             position: "relative",
             textAlign: "center",
             minHeight: "50vh", // Sử dụng minHeight để ảnh có thể dài ra khi cần thiết
-            borderRadius: "50px",
             overflow: "hidden",
           }}
         >
           <img
-            className="d-none d-md-block position-absolute w-100 h-100"
-            style={{ objectFit: "cover" }} // Mặc định là cover trên các màn hình lớn
+            className="d-none d-md-block position-absolute w-100 h-100 rounded-5"
+            style={{ objectFit: "cover" }}
             src="images/background/BG_NhungConSo.png"
             alt="Background"
           />
           <div
             className="info-husc align-self-center position-absolute top-50 start-50 translate-middle d-flex justify-content-evenly align-items-center w-100 text-white"
-            style={{ textShadow: "1px 1px 4px rgba(0, 0, 0, 0.8)" }}
+            // style={{ textShadow: "1px 1px 4px rgba(0, 0, 0, 0.8)" }}
           >
-            <div className="text-center">
-              <h1
-                style={{
-                  fontSize: "7rem",
-                }}
-                className="fw-bold m-0 display-1"
-              >
-                26
-              </h1>
-              <p className="text-uppercase fw-bold m-0 fs-4">
-                Viên chức & người lao động
-              </p>
+            <div className="item-face text-center">
+              <div className="face-1">
+                <h1
+                  style={{
+                    fontSize: "7rem",
+                  }}
+                  className="fw-bold m-0 display-1"
+                >
+                  26
+                </h1>
+                <p className="text-uppercase fw-bold m-0 fs-4">
+                  Viên chức & người lao động
+                </p>
+              </div>
+              <div className="d-none face-2">
+                <div className="text-center fs-3">
+                  <p>06 phó giáo sư tiến sĩ</p>
+                  <p>09 tiến sĩ</p>
+                  <p>10 thạc sĩ</p>
+                  <p>02 trợ giảng</p>
+                  <p>01 văn thư</p>
+                </div>
+              </div>
             </div>
 
             <BorderLight />
 
-            <div className="text-center">
-              <h1
-                style={{
-                  fontSize: "7rem",
-                }}
-                className="fw-bold m-0 display-1"
-              >
-                03
-              </h1>
-              <p className="text-uppercase fw-bold m-0 fs-4">
-                bộ môn chuyên ngành
-              </p>
+            <div className="item-face text-center">
+              <div className="face-1">
+                <h1
+                  style={{
+                    fontSize: "7rem",
+                  }}
+                  className="fw-bold m-0 display-1"
+                >
+                  03
+                </h1>
+                <p className="text-uppercase fw-bold m-0 fs-4">
+                  bộ môn chuyên ngành
+                </p>
+              </div>
+              <div className="d-none face-2">
+                <div className="text-center fs-3">
+                  <p>Công nghệ phần mềm</p>
+                  <p>Khoa học máy tính</p>
+                  <p>Mạng máy tính</p>
+                </div>
+              </div>
             </div>
 
             <BorderLight />
 
-            <div className="text-center">
-              <h1
-                style={{
-                  fontSize: "7rem",
-                }}
-                className="fw-bold m-0 display-1"
-              >
-                02
-              </h1>
-              <p className="text-uppercase fw-bold m-0 fs-4">
-                ngành đào tạo Đại học
-              </p>
+            <div className="item-face text-center">
+              <div className="face-1">
+                <h1
+                  style={{
+                    fontSize: "7rem",
+                  }}
+                  className="fw-bold m-0 display-1"
+                >
+                  02
+                </h1>
+                <p className="text-uppercase fw-bold m-0 fs-4">
+                  ngành đào tạo Đại học
+                </p>
+              </div>
+              <div className="d-none face-2">
+                <div className="text-center fs-3">
+                  <p>Công nghệ thông tin</p>
+                  <p>Kỹ thuật phần mềm</p>
+                </div>
+              </div>
             </div>
 
             <BorderLight />
 
-            <div className="text-center">
-              <h1
-                style={{
-                  fontSize: "7rem",
-                }}
-                className="fw-bold m-0 display-1"
-              >
-                02
-              </h1>
-              <p className="text-uppercase fw-bold m-0 fs-4">
-                ngành đào tạo Thạc sĩ
-              </p>
+            <div className="item-face text-center">
+              <div className="face-1">
+                <h1
+                  style={{
+                    fontSize: "7rem",
+                  }}
+                  className="fw-bold m-0 display-1"
+                >
+                  02
+                </h1>
+                <p className="text-uppercase fw-bold m-0 fs-4">
+                  ngành đào tạo Thạc sĩ
+                </p>
+              </div>
+              <div className="d-none face-2">
+                <div className="text-center fs-3">
+                  <p>Khoa học máy tính</p>
+                  <p>Quản lí công nghệ thông tin</p>
+                </div>
+              </div>
             </div>
 
             <BorderLight />
 
-            <div className="text-center">
-              <h1
-                style={{
-                  fontSize: "7rem",
-                }}
-                className="fw-bold m-0 display-1"
-              >
-                01
-              </h1>
-              <p className="text-uppercase fw-bold m-0 fs-4">
-                ngành đào tạo Tiến sĩ
-              </p>
+            <div className="item-face text-center pe-2">
+              <div className="face-1">
+                <h1
+                  style={{
+                    fontSize: "7rem",
+                  }}
+                  className="fw-bold m-0 display-1"
+                >
+                  01
+                </h1>
+                <p className="text-uppercase fw-bold m-0 fs-4">
+                  ngành đào tạo Tiến sĩ
+                </p>
+              </div>
+              <div className="d-none face-2">
+                <div className="text-center fs-3">
+                  <p>Khoa học máy tính</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </Row>
-
-        <Row
-          className="relative my-3"
-          style={{
-            position: "relative",
-            textAlign: "center",
-            minHeight: "50vh", // Sử dụng minHeight để ảnh có thể dài ra khi cần thiết
-            borderRadius: "50px",
-            overflow: "hidden",
-          }}
-        >
-          <img
-            className="d-none d-md-block position-absolute w-100 h-100"
-            style={{ objectFit: "cover" }} // Mặc định là cover trên các màn hình lớn
-            src="images/background/BG_NhungConSo.png"
-            alt="Background"
-          />
-          <div
-            className="px-5 fw-bold data-husc align-self-center position-absolute top-50 start-50 translate-middle d-flex justify-content-evenly align-items-center w-100 text-white"
-            style={{ textShadow: "1px 1px 4px rgba(0, 0, 0, 0.8)" }}
-          >
-            <ul className="text-center fs-3">
-              <li>06 phó giáo sư tiến sĩ</li>
-              <li>09 tiến sĩ</li>
-              <li>10 thạc sĩ</li>
-              <li>02 trợ giảng</li>
-              <li>01 văn thư</li>
-            </ul>
-
-            <BorderLight />
-
-            <ul className="text-center fs-3">
-              <li>Công nghệ phần mềm</li>
-              <li>Khoa học máy tính</li>
-              <li>Mạng máy tính</li>
-            </ul>
-
-            <BorderLight />
-
-            <ul className="text-center fs-4">
-              <li>Công nghệ thông tin</li>
-              <li>Kỹ thuật phần mềm</li>
-            </ul>
-
-            <BorderLight />
-
-            <ul className="text-center fs-3">
-              <li>Khoa học máy tính</li>
-              <li>Quản lí công nghệ thông tin</li>
-            </ul>
-
-            <BorderLight />
-
-            <ul className="text-center fs-3">
-              <li>Khoa học máy tính</li>
-            </ul>
           </div>
         </Row>
       </Container>
