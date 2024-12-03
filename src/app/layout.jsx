325422d27d5bcf33b9d51b3479e83f8409d65470
XT Lenovo <tranxuantruong420@gmail.com>
2024-12-03 22:15:30 +0700

    [REUPDATE]

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-"use client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/config.css";
@@ -18,8 +17,43 @@ export default function RootLayout({ children }) {
 
   return (
     <html suppressHydrationWarning={true} lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <title>IT HUSC - 30 years anniversary</title>
+      <Head>
+        <meta
+          name="description"
+          content="Chào mừng kỷ niệm 30 năm thành lập Khoa Công nghệ Thông tin, Trường Đại học Khoa học - Đại học Huế"
+        />
+        <meta
+          name="keywords"
+          content="Kỷ niệm 30 năm, Khoa Công nghệ Thông tin, Đại học Khoa học, Đại học Huế, anniversary"
+        />
+        <meta
+          name="author"
+          content="Khoa Công nghệ Thông tin, Trường Đại học Khoa học, Đại học Huế"
+        />
+        <meta
+          property="og:title"
+          content="Kỷ niệm 30 năm Khoa Công nghệ Thông tin - Trường Đại học Khoa học"
+        />
+        <meta
+          property="og:description"
+          content="Cùng nhau nhìn lại hành trình 30 năm phát triển của Khoa CNTT, Trường Đại học Khoa học, Đại học Huế."
+        />
+        <meta property="og:image" content="/images/logo/logo.png" />{" "}
+        {/* Đảm bảo thêm một đường dẫn ảnh cho Open Graph */}
+        <meta property="og:url" content="https://it.husc.edu.vn/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Kỷ niệm 30 năm Khoa Công nghệ Thông tin - Trường Đại học Khoa học"
+        />
+        <meta
+          name="twitter:description"
+          content="Cùng nhau nhìn lại hành trình 30 năm phát triển của Khoa CNTT, Trường Đại học Khoa học, Đại học Huế."
+        />
+        <meta name="twitter:image" content="/images/logo/logo.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>IT HUSC - 30 years anniversary</title>
+      </Head>
       <body>
         {children}
         <ScrollToTop />
